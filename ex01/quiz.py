2033e6183d0a17b909ce8d9b2e3a71dd80898b2f
tomo <c0a21008c9@edu.teu.ac.jp>
2022-06-14 16:14:21 +0900

    クイズ#2

--- ex01/quiz.py
+++ ex01/quiz.py
@@ -1,25 +1,25 @@
 import random
 
 def main():
-    mondai_bangou=random.randint(0,3)
-    shutudai(mondai_bangou)
-    kaito(mondai_bangou)
+    question_number=random.randint(0,3)
+    shutudai(question_number)
+    kaito(question_number)
 
 def shutudai(x):
     q1="サザエの旦那の名前は？"
     q2="カツオの妹の名前は？"
     q3="タラオはカツオから見てどんな関係？"
     q=[q1,q2,q3]
-    mondai=q.pop(x)
-    print(mondai)
+    question=q.pop(x)
+    print(question)
 def kaito(x):
     a1=["マスオ","ますお"]
     a2=["ワカメ","わかめ"]
     a3=["甥","おい","甥っ子","おいっこ"]
     a=[a1,a2,a3]
-    kaitou=input("答えるんだ:")
-    kotae=a.pop(x)
-    result = kaitou in kotae
+    answer=input("答えるんだ:")
+    correct_answer=a.pop(x)
+    result = answer in correct_answer
     if result==True:
         print("正解")
     else:
