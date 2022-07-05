806c7e4f973db7fda54703267de241dd4c2bf826
tomo <c0a21008c9@edu.teu.ac.jp>
2022-07-05 16:27:06 +0900

    ４限終了時点

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -1,9 +1,18 @@
 import pygame as pg
 import sys
 import random
+import tkinter as tk
+import tkinter.messagebox as tkm
+import numpy as np
+import IPython
+from pygame import mixer
 
 def main():
     clock=pg.time.Clock()
+
+    mixer.init()        #初期化
+    mixer.music.load("fig/BGM.mp3") #BGMの音声ファイルの呼び出し
+    mixer.music.play(1000) #再生回数
 
 #練習２　背景
     pg.display.set_caption("逃げろ！こうかとん")
@@ -14,10 +23,13 @@ def main():
     screen_sfc.blit(bgimg_sfc, bgimg_rct)#表示
 
 #練習３　こうかとん
-    kking_sfc=pg.image.load("fig/6.png")#画像を取得
+    num=random.randint(0,9)#こうかとんをランダムで表示するためのランダム関数
+    kking_sfc=pg.image.load(f"fig/{num}.png")#画像を取得
     kkimg_sfc=pg.transform.rotozoom(kking_sfc, 0, 2.0)#２倍にズーム
     kking_rct=kking_sfc.get_rect()
-    kking_rct.center=900,400#位置を設定
+    koukatonx=random.randint(200,1400)#こうかとんの初期位置のx座標をランダムに表示するためのランダム関数
+    koukatony=random.randint(200,700)#こうかとんの初期位置のｙ座標をランダムに表示するためのランダム関数
+    kking_rct.center=koukatonx, koukatony #位置を設定
     screen_sfc.blit(kkimg_sfc,kking_rct)#表示
 #練習５爆弾
     boming_sfc=pg.Surface((20,20))
@@ -61,10 +73,24 @@ def main():
 
 #練習8
         if kking_rct.colliderect(boming_rct):
+            game_over()
+            tkm.showwarning("ゲームオーバー","残念！また挑戦してね")#ゲームオーバー文の表示
             return
 
         pg.display.update()
         clock.tick(1000)
+
+def game_over(): #ゲームオーバーの際の効果音の追加
+    mixer.init()#初期化
+    mixer.music.load("fig/poka.mp3")#音声ファイルの呼び出し
+    mixer.music.play(1)#再生回数
+
+    #音声自作の際に利用
+    #rate=4800
+    #d=1.0
+    #t=np.linspace(0.,d,int(rate*d))
+    #x=np.sin(2.0*np.pi*440.0*t)
+    #IPython.display.Audio(x, rate=rate, autoplay=True)
 
 def check_bound(rct, scr_rct):
     '''
